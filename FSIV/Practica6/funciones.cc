954d5e26c3522fe6ac8c108e86c95e6d5e3e9cc0
RubenMZ <i32mezar@uco.es>
2015-12-16 16:49:15 +0100

    Algunos cambios

--- FSIV/Practica6/funciones.cc
+++ FSIV/Practica6/funciones.cc
@@ -1,7 +1,7 @@
 #include "funciones.h"
 
 
-void guardaFichero(const Mat &image, const vector< vector<Point> > & v, string fichero){
+void guardaFichero(const Mat &image, const vector< Point > & v, string fichero){
 	
 	ofstream file;
 
@@ -9,44 +9,99 @@ void guardaFichero(const Mat &image, const vector< vector<Point> > & v, string f
 
 	for (unsigned int i = 0; i < v.size(); ++i)
 	{
-		for (unsigned int j = 0; j < v[i].size(); ++j)
-		{
-			file<<v[i][j].x<<" "<<image.cols-v[i][j].y<<endl;
-		}
-		file<<" "<<endl;
+			file<<v[i].x<<" "<<image.cols-v[i].y<<endl;
 	}
 
 	file.close();
 }
 
 
-void getEstadisticos(const vector< Point > &v, Stat &datos){
+void getEstadisticos(const Mat &dst, const vector< Point > &v, Stat &datos){
 
 	Stat prueba;
-	prueba.longitud=0;
-	prueba.area=0;
-	Point pixelAux;
+	Rect aux;
+	float aux1;
+	vector< Point > aux2;
+	Point2f center;
+	Point punto, puntoAux;
+	Mat out, auxMat;
 
-	for (unsigned int i = 1; i < v.size(); ++i)
+	aux= boundingRect(v);
+
+	prueba.longitud=arcLength(v,true);
+	prueba.area=contourArea(v,false);
+	prueba.compacidad= prueba.area/ pow( prueba.longitud,2 );
+	prueba.rectangulo= minAreaRect(v);
+
+	if (prueba.rectangulo.size.width<prueba.rectangulo.size.height)
 	{
-		pixelAux=v[i];
-		if(v[i].x==v[i-1].x || v[i].y == v[i-1].y)
-			prueba.longitud+=1;
-		else
-			prueba.longitud+=sqrt(2);
+		prueba.excentricidad = prueba.rectangulo.size.height /prueba.rectangulo.size.width;
+	}else{
+		prueba.excentricidad = prueba.rectangulo.size.width / prueba.rectangulo.size.height;
+	}
 
-		for (int j = 0; j < v.size(); ++j)
+	prueba.diametro= getDiametro(v);
+
+	convexHull(v , aux2);
+	aux1= contourArea(aux2);
+	prueba.ocupacion=prueba.area/(aux.width*aux.height);
+	prueba.ocupacion_convexa=aux1/(aux.width*aux.height);
+	prueba.solidez = float(prueba.area)/aux1;
+
+	getFourier(v, prueba.fourier);
+
+	datos=prueba;
+}
+
+
+void getFourier(const vector< Point> &v, vector< float > &fourier)   
+{   
+	int M = getOptimalDFTSize(v.size());
+	vector< Point2f > contorno;
+	int aux=0;
+
+	 for(int i = 0; i<M; i++){
+	 	contorno.push_back(v[i%v.size()]);
+	 }
+
+	dft(contorno, contorno);
+       
+	for (int i = 0; i < contorno.size(); ++i)
+	{
+		fourier.push_back(sqrt( pow(contorno[i].x,2)+ pow(contorno[i].y,2) ));
+		if(fourier[i]>aux)
+			aux=fourier[i];
+	}
+
+ 	for (int i = 0; i < contorno.size()-1; ++i)
+ 	{
+ 		fourier[i]=fourier[i+1];
+ 	}
+
+	for (int i = 0; i < contorno.size(); ++i)
+	{
+		fourier[i]/=aux;
+	}
+
+} 
+
+
+float getDiametro(const vector< Point > & v){
+	float diametro=0;
+	Point punto;
+
+	for (int i = 0; i < v.size()-1; ++i)
+	{
+		punto = v[i];
+		for (int j = i+1; j < v.size(); ++j)
 		{
-			if(pixelAux.x==v[j].x)
-				prueba.area+=abs(pixelAux.y-v[j].y);
+			float comp = sqrt( pow(punto.x-v[j].x,2)+pow(punto.y-v[j].y, 2) );
+			if (comp>diametro)
+			{
+				diametro=comp;
+			}
 		}
 	}
 
-	if (v[0].x == v[v.size()-1].x || v[0].y == v[v.size()-1].y)
-	{
-		prueba.longitud+=1;
-	}else{
-		prueba.longitud+=sqrt(2);
-	}
-	datos=prueba;
+	return diametro;
 }
